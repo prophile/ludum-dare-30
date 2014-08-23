57df2e6db8bf56237703a4e0e502e70ac383c185
Alistair Lynn <arplynn@gmail.com>
2014-08-23 15:15:43 +0100

    Add toroidal wrapping for the player ship

--- core/src/uk/co/alynn/games/ld30/world/Constants.java
+++ core/src/uk/co/alynn/games/ld30/world/Constants.java
@@ -2,4 +2,5 @@ package uk.co.alynn.games.ld30.world;
 
 public final class Constants {
     public static final float SPEED = 100.0f;
+    public static final float WRAP_BUFFER = 30.0f;
 }
--- core/src/uk/co/alynn/games/ld30/world/PlayerShip.java
+++ core/src/uk/co/alynn/games/ld30/world/PlayerShip.java
@@ -1,5 +1,7 @@
 package uk.co.alynn.games.ld30.world;
 
+import com.badlogic.gdx.Gdx;
+
 public class PlayerShip {
     private final float m_x, m_y;
     private final float m_heading;
@@ -31,8 +33,20 @@ public class PlayerShip {
     public PlayerShip update(float dt) {
         float xPrime = m_x;
         float yPrime = m_y;
-        xPrime += dt*Constants.SPEED*Math.sin(m_heading);
-        yPrime += dt*Constants.SPEED*Math.cos(m_heading);
+        xPrime += dt*Constants.SPEED*Math.cos(m_heading);
+        yPrime += dt*Constants.SPEED*Math.sin(m_heading);
+        xPrime = wrap(xPrime, Gdx.graphics.getWidth());
+        yPrime = wrap(yPrime, Gdx.graphics.getHeight());
         return new PlayerShip(xPrime, yPrime, m_heading);
+    }
+
+    private static float wrap(float newValue, float maximum) {
+        while (newValue > (maximum + Constants.WRAP_BUFFER)) {
+            newValue -= (maximum + 2*Constants.WRAP_BUFFER);
+        }
+        while (newValue < -Constants.WRAP_BUFFER) {
+            newValue += (maximum + 2*Constants.WRAP_BUFFER);
+        }
+        return newValue;
     }
 }
