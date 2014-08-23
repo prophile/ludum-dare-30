2aaae0a3cedb679de749efb2d9ac1d9d5d9901ad
Alistair Lynn <arplynn@gmail.com>
2014-08-23 14:37:37 +0100

    Initial commit

---
+++ desktop/src/uk/co/alynn/games/ld30/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.co.alynn.games.ld30.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.co.alynn.games.ld30.SpaceHams;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceHams(), config);
+	}
+}
