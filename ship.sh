77e34bed476731b6753eeaff01a06f26c5e7c76e
Alistair Lynn <arplynn@gmail.com>
2014-08-23 19:06:12 +0100

    Use rsync in ship.sh

--- ship.sh
+++ ship.sh
@@ -2,5 +2,5 @@
 set -e
 set -v
 ./gradlew dist
-scp desktop/build/libs/desktop-1.0.jar alynn.co.uk:public_html/space.jar
+rsync --progress desktop/build/libs/desktop-1.0.jar alynn.co.uk:public_html/space.jar
 
