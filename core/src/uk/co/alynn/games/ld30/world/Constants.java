9eb4c322255bc266294bb1c718cf02e5b76f6142
Alistair Lynn <arplynn@gmail.com>
2014-08-24 17:43:05 +0100

    Be stricter on forward grabs

--- core/src/uk/co/alynn/games/ld30/world/Constants.java
+++ core/src/uk/co/alynn/games/ld30/world/Constants.java
@@ -7,7 +7,7 @@ public final class Constants {
     public static final float SPEED_BULLET = 600.0f;
     public static final float ASTEROID_SPEED = 60.0f;
     public static final float SECONDS_PER_TICK = 1.0f;
-    public static final float BIND_LIMIT = 5.0f;
+    public static final float BIND_LIMIT = 25.0f;
     public static final float PLANET_BIND_RADIUS = 240.0f;
     public static final float SPEED_INVADER = 80.0f;
     public static final float INVADER_HOLDOFF = 180.0f;
