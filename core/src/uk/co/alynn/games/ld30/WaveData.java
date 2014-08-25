e72dbe91c9ce0e8b05cc074da269fdeb1e22edc9
Alistair Lynn <arplynn@gmail.com>
2014-08-25 14:15:57 +0100

    Add better boss waves

--- core/src/uk/co/alynn/games/ld30/WaveData.java
+++ core/src/uk/co/alynn/games/ld30/WaveData.java
@@ -1,5 +1,7 @@
 package uk.co.alynn.games.ld30;
 
+import com.badlogic.gdx.math.MathUtils;
+
 public abstract class WaveData {
     public static Wave getWave(int wave) {
         switch (wave) {
@@ -132,17 +134,80 @@ public abstract class WaveData {
                 }
             };
         default:
-            return new Wave() {
-                @Override
-                protected boolean tick(int tick) {
-                    switch (tick) {
-                    case 2:
-                        spawnDestroyer(50, -5);
-                        break;
-                    }
-                    return tick >= 2;
-                }
-            };
+            if (wave % 5 == 0) {
+                return spawnGenericBossWave((wave / 5) - 1);
+            } else {
+                return spawnGenericWave((wave - 5) / 5);
+            }
         }
+    }
+
+    private static Wave spawnGenericWave(int i) {
+        final int difficulty = i + 1;
+        return new Wave() {
+            @Override
+            protected boolean tick(int tick) {
+                if (tick == 0)
+                    return false;
+                if (tick > difficulty*3)
+                    return true;
+                if (tick == 6) {
+                    while (MathUtils.random(2) == 1) {
+                        spawnDestroyer((float)MathUtils.random(0, 100), -5.0f);
+                    }
+                }
+                if (tick % 3 == 0) {
+                    for (int i = 0; i < 2; ++i) {
+                        spawnInvader((float)MathUtils.random(0, 100), 105.0f, MathUtils.random(2));
+                    }
+                } else {
+                    int nasteroids = MathUtils.random(difficulty);
+                    for (int i = 0; i < nasteroids; ++i) {
+                        if (MathUtils.random(1) == 1) {
+                            spawnAsteroid(-5.0f, MathUtils.random(0.0f, 100.0f), MathUtils.random(-50.0f, 50.0f));
+                        } else {
+                            spawnAsteroid(105.0f, MathUtils.random(0.0f, 100.0f), MathUtils.random(130.0f, 230.0f));
+                        }
+                    }
+                }
+                return false;
+            }
+        };
+    }
+
+    private static Wave spawnGenericBossWave(final int difficulty) {
+        int wave1 = 1 + MathUtils.random(difficulty + 3);
+        int wave2 = 1 + MathUtils.random(difficulty + 2);
+        final int destroyers = MathUtils.random(difficulty);
+        final int targetPlanet = MathUtils.random(2);
+        final int wave1Start = (100 / (wave1 + 1));
+        final int wave1End = wave1*wave1Start;
+        final int wave1Increment = wave1Start;
+        final int wave2Start = (100 / (wave2 + 1));
+        final int wave2End = wave2*wave2Start;
+        final int wave2Increment = wave2Start;
+        return new Wave() {
+
+            @Override
+            protected boolean tick(int tick) {
+                if (tick == 5) {
+                    for (int i = wave1Start; i <= wave1End; i += wave1Increment) {
+                        spawnInvader(-5.0f, (float)i, targetPlanet);
+                        spawnInvader(105.0f, (float)i, targetPlanet);
+                    }
+                } else if (tick == 10) {
+                    for (int i = 0; i < destroyers; ++i) {
+                        spawnDestroyer(MathUtils.random(20, 80), 105f);
+                    }
+                } else if (tick == 15) {
+                    for (int i = wave2Start; i <= wave2End; i += wave2Increment) {
+                        spawnInvader(-5.0f, (float)i, targetPlanet);
+                        spawnInvader(105.0f, (float)i, targetPlanet);
+                    }
+                }
+                return tick >= 15;
+            }
+            
+        };
     }
 }
